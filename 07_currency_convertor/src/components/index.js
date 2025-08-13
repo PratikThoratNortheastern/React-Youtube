7d9ee40444c470f75003f121bc49339f23eefb6d
Pratik Thorat <thorat.p@northeastern.edu>
2025-08-13 19:27:15 -0400

    finished project

---
+++ 07_currency_convertor/src/components/index.js
@@ -0,0 +1,5 @@
+// The crux of the matter is that we have to bring all our components here in this index file and import just this one file so our App.jsx file is litte less messier
+
+import InputBox from "./InputBox";
+
+export { InputBox };
