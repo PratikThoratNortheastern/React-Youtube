9ebdfee9532fd0262d8b5d1e9fd5de507d760f96
Pratik Thorat <thorat.p@northeastern.edu>
2026-04-08 04:14:22 -0400

    Imports latest example files

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,10 +1,25 @@
 import { Button } from "./Button";
+import { Hello, HelloWithoutJsx } from "./Hello";
 import { Welcome } from "./Welcome";
+import { ProductDetails } from "./ProductDetails";
 
 function App() {
   return (
     <div>
+      <ProductDetails
+        title="Laptop"
+        inStock={true}
+        description="This is a gaming laptop with great graphics"
+        price={1299.99}
+      />
+      <ProductDetails
+        title="Coffee"
+        description="Butter Pecan Coffee"
+        price={10.62}
+      />
       <h1>React 19 Course!</h1>
+      <Hello />
+      <HelloWithoutJsx />
       <Welcome />
       <Button />
     </div>
